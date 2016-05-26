3d9c261f526854bf686a96f356c1bfdf94ba7d5a
kevin2106 <keviee007@live.nl>
2016-05-26 14:26:17 +0200

    3.3

--- Visualisatie-Opdracht2/src/main/java/com/mycompany/visualisatie/opdracht2/Window.java
+++ Visualisatie-Opdracht2/src/main/java/com/mycompany/visualisatie/opdracht2/Window.java
@@ -24,6 +24,11 @@ public class Window extends PApplet {
     int width = 300;
     int length = 300;
     
+    int x1, x2, x3;
+    int y1, y2, y3;
+    
+    int[] regressionValue;
+    
     int[] minMaxEig1;
     float[] minMaxEig2;
     ArrayList<DataModel> mappedData;
@@ -35,6 +40,8 @@ public class Window extends PApplet {
         try {
             data = dr.parser();
             mappedData = mapData(data, position, length, width);
+            regressionValue = calcRegressionLine(data);
+            calcAxis();
         } catch (IOException ex) {
             Logger.getLogger(Window.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ParseException ex) {
@@ -44,10 +51,29 @@ public class Window extends PApplet {
 
     public void draw() {
         drawScatterGraph(position, length, width, "Eig1", "Eig2");
-        System.out.println(mouseX + " : " + mouseY);
-
     }
 
+    private void calcAxis() {
+        ArrayList<Integer> eig1 = new ArrayList();
+        ArrayList<Float> eig2 = new ArrayList();
+
+        for (DataModel datas : data) {
+            eig1.add(datas.getEig1());
+            eig2.add(datas.getEig2());
+        }
+
+        int[] minMaxEig1 = getMinMaxInteger(eig1);
+        float[] minMaxEig2 = getMinMaxFloat(eig2);
+        
+        x1 = minMaxEig1[0];
+        x2 = ((minMaxEig1[0] + minMaxEig1[1]) / 2);
+        x3 = minMaxEig1[1];
+        
+        y1 = (int) minMaxEig2[0];
+        y2 = (int) ((minMaxEig2[0] + minMaxEig2[1]) / 2);
+        y3 = (int) minMaxEig2[1];
+    }
+    
     public void drawScatterGraph(int[] position, int length, int width, String xLabel, String yLabel) {
         line(position[0], position[1], position[0] + width, position[1]);
         line(position[0], position[1], position[0], position[1] - length);
@@ -57,13 +83,13 @@ public class Window extends PApplet {
         text(xLabel, position[0] + width + 10, position[1]);
         text(yLabel, position[0], position[1] - length - 10);
 
-        text(0, position[0] - 10, position[1] + 15);
+        text(x1, position[0], position[1] + 15);
+        text(x2, position[0] + (0.50f * width), position[1] + 15);
+        text(x3, position[0] + width, position[1] + 15);
 
-        text(0, position[0] + (0.50f * width), position[1] + 15);
-        text(0, position[0] + width, position[1] + 15);
-
-        text(1000, position[0] - 15, position[1] - (0.5f * length));
-        text(0, position[0] - 15, position[1] - length);
+        text(y1, position[0] - 30, position[1]);
+        text(y2, position[0] - 30, position[1] - (0.5f * length));
+        text(y3, position[0] - 30, position[1] - length);
         
 
         for (DataModel mapData : mappedData) {
@@ -72,8 +98,72 @@ public class Window extends PApplet {
 
         }
 
+        drawRegressionLine(position, length, width, minMaxEig1, minMaxEig2);
+    }
+    
+    public int[] calcRegressionLine(ArrayList<DataModel> data){
+        ArrayList<Integer> eig1 = new ArrayList();
+        ArrayList<Float> eig2 = new ArrayList();
+          
+        for (DataModel datas : data) {
+            eig1.add(datas.getEig1());
+            eig2.add(datas.getEig2());
+        }
+        
+        int[] minMaxX = getMinMaxInteger(eig1);
+        float[] minMaxY = {0, 0};
+        
+        for(DataModel datas : data){
+            if(datas.getEig1() == minMaxX[0]){
+                minMaxY[0] = datas.getEig2();    
+            }
+            
+            if(datas.getEig1() == minMaxX[1]){
+                minMaxY[1] = datas.getEig2();
+            }
+        }      
+        
+        int x = eig1.get(0);
+        float y = eig2.get(0);
+        
+        float a = ((minMaxY[1] - minMaxY[0]) / (minMaxX[1] - minMaxX[0]));
+        float temp = a * x; 
+        float b = (temp * -1) + y;
+
+        
+        
+        int beginPosY =  (int) ((a * minMaxX[0]) + b);
+        System.out.println(beginPosY);
+        int temp2 = (int) (minMaxY[0] - b);
+        int beginPosX = (int) (temp2 / a);
+        //int beginPosX =  (int) ((a * minMaxY[0]) + b);
+        
+        int endPosY =  (int) ((a * minMaxX[1]) + b);
+        System.out.println(beginPosY);
+        int temp3 = (int) (minMaxY[1] - b);
+        int endPosX = (int) (temp3 / a);
+
+     
+        int[] regressionValues = {beginPosX, beginPosY, endPosX, endPosY};
+        System.out.println(regressionValues[0] + " : " + regressionValues[1]);
+        
+        return regressionValues;
     }
 
+    public void drawRegressionLine(int[] position, int length, int width, int[] minMaxEig1, float[] minMaxEig2){
+  
+        
+        int mappedBeginX = (int) map(regressionValue[0], minMaxEig1[0], minMaxEig1[1], position[0], position[0] + width);
+        int mappedBeginY = (int) map(regressionValue[1], minMaxEig2[0], minMaxEig2[1], position[1], position[1] - length);
+        
+        int mappedEndX = (int) map(regressionValue[2], minMaxEig1[0], minMaxEig1[1], position[0], position[0] + width);
+        int mappedEndY = (int) map(regressionValue[3], minMaxEig2[0], minMaxEig2[1], position[1], position[1] - length);
+        
+        System.out.println(mappedBeginX + " : " + mappedBeginY);
+        
+        line(mappedBeginX, mappedBeginY, mappedEndX, mappedEndY);
+    }
+    
     public ArrayList<DataModel> mapData(ArrayList<DataModel> data, int[] position, int length, int width) {
         ArrayList<Integer> eig1 = new ArrayList();
         ArrayList<Float> eig2 = new ArrayList();
